ef3e7c06de51fd4cb92413b2661c8ff7f8fc64e5
asiphilengutyana16-byte <asiphilengutyana8@gmail.com>
2026-07-09 00:27:10 +0200

    Create hello.py

---
+++ hello.py
@@ -0,0 +1,7 @@
+name = "Asiphile"
+goal = "Software Engineer in Dubai"
+salary_goal = "150000 AED"
+
+print(f"Hi, I'm {name}")
+print(f"My goal is to be a {goal}")
+print(f"Making {salary_goal} per year")
