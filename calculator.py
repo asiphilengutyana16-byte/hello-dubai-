c4dde8ebe209d2eed546fce4b23f1b2aa3ab7819
asiphilengutyana16-byte <asiphilengutyana8@gmail.com>
2026-07-09 00:35:19 +0200

    Create calculator.py

---
+++ calculator.py
@@ -0,0 +1,9 @@
+print("=== ASIPHILE'S CALCULATOR ===")
+num1 = float(input("First number: "))
+num2 = float(input("Second number: "))
+
+print("\nResults:")
+print("Sum:", num1 + num2)
+print("Minus:", num1 - num2)
+print("Times:", num1 * num2)
+print("Divide:", num1 / num2)
